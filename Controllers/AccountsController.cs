682ea6200f516f45cd8ecc0109de381fdf4bd3c7
Richa19rai <richa19rai@gmail.com>
2025-05-06 01:14:49 +1000

    Test Module added

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankAPI.Models;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("banking/accounts")]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<AccountResponse> GetAccounts()
+        {
+            // In a real application, this would come from a database
+            var accounts = new List<Account>
+            {
+                new Account 
+                { 
+                    AccountId = "12345678", 
+                    DisplayName = "Everyday Savings", 
+                    AccountType = "SAVINGS",
+                    AccountStatus = "OPEN",
+                    Currency = "AUD",
+                    OpeningDate = "2020-01-01",
+                    AvailableBalance = 1250.42m
+                },
+                new Account 
+                { 
+                    AccountId = "87654321", 
+                    DisplayName = "Investment Account", 
+                    AccountType = "INVESTMENT",
+                    AccountStatus = "OPEN",
+                    Currency = "AUD",
+                    OpeningDate = "2019-05-20",
+                    AvailableBalance = 15720.00m
+                }
+            };
+
+            var response = new AccountResponse
+            {
+                Data = accounts,
+                Links = new Links
+                {
+                    Self = "https://api.bigpurplebank.com/banking/accounts",
+                    First = "https://api.bigpurplebank.com/banking/accounts?page=1",
+                    Last = "https://api.bigpurplebank.com/banking/accounts?page=1"
+                },
+                Meta = new Meta
+                {
+                    TotalRecords = accounts.Count,
+                    TotalPages = 1
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
--- Models/Account.cs
+++ Models/Account.cs
@@ -1,14 +1,33 @@
-﻿namespace BankingApi.Models;
-public class Account
+﻿namespace BankAPI.Models
 {
-    public string AccountId { get; set; }
-    public string DisplayName { get; set; }
-    public string AccountType { get; set; }
-    public Balance Balance { get; set; }
-}
+    public class Account
+    {
+        public string AccountId { get; set; }
+        public string DisplayName { get; set; }
+        public string AccountType { get; set; }
+        public string AccountStatus { get; set; }
+        public string Currency { get; set; }
+        public string OpeningDate { get; set; }
+        public decimal AvailableBalance { get; set; }
+    }
 
-public class Balance
-{
-    public string Amount { get; set; }
-    public string Currency { get; set; }
+    public class AccountResponse
+    {
+        public List<Account> Data { get; set; }
+        public Links Links { get; set; }
+        public Meta Meta { get; set; }
+    }
+
+    public class Links
+    {
+        public string Self { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+
+    public class Meta
+    {
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+    }
 }
